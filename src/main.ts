2a4b929078dba57803702781ec07a467de85db2f
jellyb3e <itsjuliamari@gmail.com>
2025-10-15 04:27:47 +0000

    added basic drawing functionality and clear button (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,35 @@ document.body.innerHTML = `
   <h1>doodlin' pad</h1>
 `;
 document.body.append(canvas);
+
+const ctx = canvas.getContext("2d");
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (ctx && cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  if (ctx) ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
